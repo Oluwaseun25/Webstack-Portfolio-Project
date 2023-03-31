5ebfddfb7dc4be7e3b7463bdf5892d950f1f131c
Oluwaseun25 <Samuelawe842@gmail.com>
2023-03-31 01:34:02 +0000

    js file updated

--- script.js
+++ script.js
@@ -14,3 +14,37 @@ if (close) {
     })  
 }
 
+const scrollToTopButton = document.getElementById("scrollToTopButton");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopButton.style.display = "block";
+  } else {
+    scrollToTopButton.style.display = "none";
+  }
+};
+
+function scrollToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+function scrollToTop() {
+    const duration = 500; // duration of the animation in milliseconds
+    const startingY = window.pageYOffset;
+    const diff = startingY > 0 ? -startingY : 0;
+  
+    let start;
+  
+    window.requestAnimationFrame(function step(timestamp) {
+      if (!start) start = timestamp;
+      const timeElapsed = timestamp - start;
+      const percent = Math.min(timeElapsed / duration, 1);
+      window.scrollTo(0, startingY + diff * percent);
+      if (timeElapsed < duration) {
+        window.requestAnimationFrame(step);
+      }
+    });
+  }
+  
